9be4a63288e8b0dbe629eef38d2e353ccea11e4b
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-08 20:05:53 +0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SwiftUI-T1
- 
+
+[Source](https://designcode.io/swiftui-handbook-visual-editor-in-xcode)
+
+<pre>
+<img src="preview/example1.png" width="300">
+</pre>
+
+---
+
+```
+Copyright 2022 M. Fadli Zein
+```
--- SwiftUI T1/ContentView.swift
+++ SwiftUI T1/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center, spacing: 8.0) {
+            Circle()
+                .frame(width: 40.0, height: 40.0)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+            Text("Hallo GZeinNumer")
+                .font(.title)
+            Text("Belajar Swift")
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+        .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
